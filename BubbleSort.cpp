e6c476ac00a780e5b19cf7d4464386ffc1843e3f
ShoaibKakal <fa19-bcs-109@cuilahore.edu.pk>
2021-06-17 12:55:49 +0500

    BubbleSort update

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,6 +2,19 @@
 // Created by Shoaib on 6/17/2021.
 //
 
+
+/*
+	n = array size;
+	No. of passes = n-1;
+	No. of comparsions = 1+2+3+...+n-1 = n(n-1)/2 = 0(n^2);
+	No. of Swaps = 1+2+3+4...+n-1 = n(n-1)/2 = 0(n^2);
+	Adaptive = true;
+	Stable = true;
+	
+	Max. Time Complexity = n^2;
+	Min. Time Complexity = n;(if array is already sorted)
+
+*/
 #include "iostream"
 using namespace std;
 int main()
