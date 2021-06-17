1bd2eb1e765bc5744b5620c7a306f4d2626532fd
Shoaib Khalid <shoaibkakil@gmail.com>
2021-06-17 12:57:28 +0500

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -6,13 +6,13 @@
 /*
 	n = array size;
 	No. of passes = n-1;
-	No. of comparsions = 1+2+3+...+n-1 = n(n-1)/2 = 0(n^2);
-	No. of Swaps = 1+2+3+4...+n-1 = n(n-1)/2 = 0(n^2);
+	No. of comparsions = 1+2+3+...+n-1 = n(n-1)/2 = O(n^2);
+	No. of Swaps = 1+2+3+4+...+n-1 = n(n-1)/2 = O(n^2);
 	Adaptive = true;
 	Stable = true;
 	
-	Max. Time Complexity = n^2;
-	Min. Time Complexity = n;(if array is already sorted)
+	Max. Time Complexity = O(n^2);
+	Min. Time Complexity = O(n); (if array is already sorted)
 
 */
 #include "iostream"
